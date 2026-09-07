51fbad9095b3ae36da375858c09d1f0b69772e45
danidimitrovaa1 <danidimitrovaa1@gmail.com>
2026-09-07 14:48:08 +0300

    Add dedicated handler methods & clean up code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, computed } from '@angular/core';
 import { RecipeCard } from './recipe-card/recipe-card';
 import { RecipeService } from './services/recipe';
+import { FilterType, Recipe } from './models/recipe.model';
 
 @Component({
   imports: [RecipeCard],
@@ -11,16 +12,26 @@ import { RecipeService } from './services/recipe';
 export class App {
   protected readonly title = signal('Smart Recipe Box 🥗');
 
-  protected readonly recipeService = inject(RecipeService);
+  private readonly recipeService = inject(RecipeService);
+
+  readonly allRecipes = computed<Recipe[]>(() => this.recipeService.recipes());
+
+  readonly filteredRecipes = computed<Recipe[]>(() => this.recipeService.filteredRecipes());
+
+  readonly favoriteRecipesCount = computed<number>(() => this.recipeService.favoriteCount());
+
+  readonly currentFilter = computed<FilterType>(() => this.recipeService.currentFilter());
 
   protected onAddRecipe(
+    event: SubmitEvent,
     recipeModal: HTMLDialogElement,
     titleInput: HTMLInputElement,
     descriptionInput: HTMLTextAreaElement,
     timeInput: HTMLInputElement,
     difficultySelect: HTMLSelectElement,
   ) {
-    // 1. Prevent page reload - in the HTML form tag on submit
+    // 1. Prevent page reload
+    event.preventDefault();
 
     // 2. Gather input values
     // 3. Call the addRecipe function from the service
@@ -38,6 +49,30 @@ export class App {
 
     // 5. Close the modal
     // 6. Call it in the HTML submit form tag
+    recipeModal.close();
+  }
+
+  setSearchQuery(value: string) {
+    this.recipeService.searchQuery.set(value);
+  }
+
+  setFilter(filter: FilterType) {
+    this.recipeService.setFilter(filter);
+  }
+
+  onToggleFavorite(recipeId: number) {
+    this.recipeService.toggleFavorite(recipeId);
+  }
+
+  onDeleteRecipe(recipeId: number) {
+    this.recipeService.deleteRecipe(recipeId);
+  }
+
+  showRecipeModal(recipeModal: HTMLDialogElement) {
+    recipeModal.showModal();
+  }
+
+  hideRecipeModal(recipeModal: HTMLDialogElement) {
     recipeModal.close();
   }
 }
